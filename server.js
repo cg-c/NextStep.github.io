8ce167db9c33da6b6747d83c59535a8e94e2daca
cg-c <cally.cg44@gmail.com>
2022-11-17 01:40:43 -0500

    Linked All Pages
    Added all industry pages and linked them all.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const PORT = 3000;
+const path = require('path');
 const app = express();
+
+app.use(express.static('public'));
 
 // app.set('views', './views');
 app.set('view engine', 'ejs');
@@ -11,6 +14,54 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about');
+})
+
+app.get('/industries', (req, res) => {
+    res.render('industries');
+})
+
+app.get('/quiz', (req, res) => {
+    res.render('quiz');
+})
+
+app.get('/industries/construction', (req, res) => {
+    res.render('industries/construction');
+})
+
+app.get('/industries/cyberSecurity', (req, res) => {
+    res.render('industries/cyberSecurity');
+})
+
+app.get('/industries/dataScience', (req, res) => {
+    res.render('industries/dataScience');
+})
+
+app.get('/industries/education', (req, res) => {
+    res.render('industries/education');
+})
+
+app.get('/industries/finance', (req, res) => {
+    res.render('industries/finance');
+})
+
+app.get('/industries/healthCare', (req, res) => {
+    res.render('industries/healthCare');
+})
+
+app.get('/industries/iot', (req, res) => {
+    res.render('industries/iot');
+})
+
+app.get('/industries/music', (req, res) => {
+    res.render('industries/music');
+})
+
+app.get('/industries/realitySpace', (req, res) => {
+    res.render('industries/realitySpace');
+})
+
+app.get('/industries/theoretical', (req, res) => {
+    res.render('industries/theoretical');
 })
 
 app.listen(PORT, () => {
